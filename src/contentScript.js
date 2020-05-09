007cdf7629bf48f0af6be1e56c9a5cf58499f002
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-10 00:01:00 +0900

    Fix undefined error

--- src/contentScript.js
+++ src/contentScript.js
@@ -9,9 +9,11 @@ chrome.runtime.onMessage.addListener((message) => {
 const main = () => {
   window.addEventListener("beforeunload", (event) => {
     const textarea = document.getElementsByTagName("textarea")[0]
-    if (textarea && textarea.value) {
+    if (textarea) {
+      if (textarea.value) {
       event.preventDefault()
       event.returnValue = ""
+    }
     }
   })
 }
