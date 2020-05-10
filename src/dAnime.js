c10d8a46ac7158bab46b599630d048085509a96d
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-10 14:29:23 +0900

    Implement Esc shortcut in d Anime Store

--- src/dAnime.js
+++ src/dAnime.js
@@ -1,1 +1,10 @@
 "use strict"
+
+window.onload = () => {
+  window.onkeyup = (e) => {
+    if (e.key === "Escape") {
+      const closeBtn = document.getElementsByClassName("closeBtn")[0]
+      closeBtn.click()
+    }
+  }
+}
