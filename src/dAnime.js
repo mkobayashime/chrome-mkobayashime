7eacd9447edd115ad829895a893b0037a5cfd1c1
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-10 21:23:29 +0900

    Validate the url in Ctrl+Enter shortcut

--- src/dAnime.js
+++ src/dAnime.js
@@ -11,11 +11,13 @@ window.onload = () => {
   window.onkeypress = (e) => {
     if (e.ctrlKey && e.code === "Enter") {
       const curUrl = window.location.href
-      const partId = curUrl.substr(curUrl.indexOf("partId") + 7)
-      window.open(
-        `https://anime.dmkt-sp.jp/animestore/sc_d_pc?partId=${partId}`,
-        "newtab"
-      )
+      if (curUrl.indexOf("partId") !== -1) {
+        const partId = curUrl.substr(curUrl.indexOf("partId") + 7)
+        window.open(
+          `https://anime.dmkt-sp.jp/animestore/sc_d_pc?partId=${partId}`,
+          "newtab"
+        )
+      }
     }
   }
 }
