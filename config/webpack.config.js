3f638f2aa0e6922f20ce1d41145aaee5bea9d791
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-19 00:23:13 +0900

    Add manaba.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,6 +10,7 @@ const config = merge(common, {
   entry: {
     tweetdeck: PATHS.src + '/tweetdeck.js',
     dAnime: PATHS.src + '/dAnime.js',
+    manaba: PATHS.src + '/manaba.js',
     background: PATHS.src + '/background.js',
   },
 });
