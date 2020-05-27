857fd3471eec7d5912a7534b9bddc931bf559c2d
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-28 00:09:32 +0900

    [Add] Add tulips.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -11,6 +11,7 @@ const config = merge(common, {
     tweetdeck: PATHS.src + '/tweetdeck.js',
     dAnime: PATHS.src + '/dAnime.js',
     manaba: PATHS.src + '/manaba.js',
+    tulips: PATHS.src + '/tulips.js',
     background: PATHS.src + '/background.js',
   },
 });
