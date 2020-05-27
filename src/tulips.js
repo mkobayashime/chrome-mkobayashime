75ccfcdce1f9b0c47c7925dd8a860b50008e0225
mkobayashime <m.kobayashi.me@gmail.com>
2020-05-28 00:25:55 +0900

    [Update] Use MutationObserver in tulips.js

--- src/tulips.js
+++ src/tulips.js
@@ -1,29 +1,39 @@
 "use strict"
 
 window.onload = () => {
-  const promise = new Promise((resoleve) => {
-    const interval = window.setInterval(() => {
-      const rows = document.getElementsByClassName("article")
-      console.log(rows)
-      if (rows.length) {
-        window.clearInterval(interval)
-        resoleve(rows)
-      }
-    }, 250)
+  const resultsWrapper = document.getElementsByClassName("results")[0]
+  const observer = new MutationObserver(() => {
+    createATag()
+  })
+  observer.observe(resultsWrapper, {
+    childList: true
   })
 
-  promise.then((rows) => {
-    for (const row of rows) {
-      row.style.position = "relative"
-  
-      const aTag = document.createElement("a")
-      aTag.style.position = "absolute"
-      aTag.style.width = "100%"
-      aTag.style.height = "100%"
-      aTag.style.top = "0"
-      aTag.style.left = "0"
-  
-      row.appendChild(aTag)
-    }
-  })
+  const createATag = () => {
+    const promise = new Promise((resoleve) => {
+      const interval = window.setInterval(() => {
+        const rows = document.getElementsByClassName("article")
+        console.log(rows)
+        if (rows.length) {
+          window.clearInterval(interval)
+          resoleve(rows)
+        }
+      }, 250)
+    })
+
+    promise.then((rows) => {
+      for (const row of rows) {
+        row.style.position = "relative"
+    
+        const aTag = document.createElement("a")
+        aTag.style.position = "absolute"
+        aTag.style.width = "100%"
+        aTag.style.height = "100%"
+        aTag.style.top = "0"
+        aTag.style.left = "0"
+    
+        row.appendChild(aTag)
+      }
+    })
+  }
 }
