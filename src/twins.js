7cdc584b0b166bf2cdc7631ec557482ddb6cd661
mkobayashime <mkobayashime@gmail.com>
2020-07-05 17:43:27 +0900

    [Update] Implement shortcuts in bulletin board of TWINS

--- src/twins.js
+++ src/twins.js
@@ -1,1 +1,32 @@
 "use strict"
+
+window.onload = () => {
+  const url = window.location.href
+
+  if (url.indexOf("&tabId=kj") === url.length - 9) {
+    setKeijiShortcuts()
+  }
+}
+
+const setKeijiShortcuts = () => {
+  window.onkeyup = (e) => {
+    if (e.ctrlKey && e.code === "Enter") {
+      const unreadIndicators = document.getElementsByClassName("highlight_red")
+      if (unreadIndicators.length) {
+        const unreadLink = unreadIndicators[0].parentElement.children[0]
+        unreadLink.click()
+      } else {
+        const iframeDoc = document.getElementById("main-frame-if").contentWindow
+          .document
+        if (iframeDoc) {
+          const backBtn = iframeDoc.querySelector(
+            "input[onclick='backPortal();']"
+          )
+          if (backBtn) {
+            backBtn.click()
+          }
+        }
+      }
+    }
+  }
+}
