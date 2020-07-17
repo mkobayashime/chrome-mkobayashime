f5020dc4b1f867a4f7883cdc1f2a6177630fe544
Masaki Kobayashi <mkobayashime@gmail.com>
2020-07-17 16:47:53 +0900

    [Update] Enable shortcuts in musasi

--- src/musasi.js
+++ src/musasi.js
@@ -1,1 +1,38 @@
 "use strict"
+
+window.onload = () => {
+  // console.log("onload")
+
+  document.onkeydown = (e) => {
+    if (e.code === "KeyJ") {
+      const btnTrue = document.getElementsByClassName("btn_true")[0]
+      if (btnTrue) {
+        btnTrue.click()
+      }
+    }
+    if (e.code === "KeyK") {
+      const btnFalse = document.getElementsByClassName("btn_false")[0]
+      if (btnFalse) {
+        btnFalse.click()
+      }
+    }
+    if (e.code === "KeyL") {
+      const btnUnsure = document.getElementById("btn_unsure")
+      if (btnUnsure) {
+        btnUnsure.click()
+      }
+    }
+
+    if (e.code === "Enter") {
+      const ansBtnImg = document.querySelector("img[alt='正解と解説']")
+      if (ansBtnImg) {
+        ansBtnImg.parentElement.click()
+      }
+
+      const nextBtnImg = document.querySelector("img[alt='次の問題']")
+      if (nextBtnImg) {
+        nextBtnImg.parentElement.click()
+      }
+    }
+  }
+}
