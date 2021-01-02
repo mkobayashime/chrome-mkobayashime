8d8cb7709bce2e2f41a03b718d50f0a25d2b64f6
Masaki Kobayashi <contact@mkobayashi.me>
2021-01-03 00:52:01 +0900

    [Chore] Refactor entry/output of webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,22 +3,8 @@
 const merge = require("webpack-merge")
 
 const common = require("./webpack.common.js")
-const PATHS = require("./paths")
 
 // Merge webpack configuration files
-const config = merge(common, {
-  entry: {
-    quickTazugane: PATHS.src + "/quickTazugane.js",
-    tweetdeck: PATHS.src + "/tweetdeck.js",
-    dAnime: PATHS.src + "/dAnime.js",
-    manaba: PATHS.src + "/manaba.js",
-    tulips: PATHS.src + "/tulips.js",
-    twins: PATHS.src + "/twins.js",
-    niconico: PATHS.src + "/niconico.js",
-    moneytree: PATHS.src + "/moneytree.js",
-    kurashiru: PATHS.src + "/kurashiru.js",
-    disableForceDownload: PATHS.src + "/disableForceDownload.js",
-  },
-})
+const config = merge(common, {})
 
 module.exports = config
