79e5fab6f551833f34d67fb71a019a8e6d9d0cc6
Masaki Kobayashi <contact@mkobayashi.me>
2021-01-20 21:59:34 +0900

    [Update] Update scrapbox.js

--- src/contentScripts/scrapbox.js
+++ src/contentScripts/scrapbox.js
@@ -1,19 +1,30 @@
 "use strict"
 
-const projectId = window.location.href.match(
-  RegExp("^https://scrapbox.io/(?<projectId>.*)/.*$")
-).groups.projectId
+const url = window.location.href
 
-const interval = window.setInterval(() => {
-  const projectStyle = document.querySelector(
-    `link[href='/api/code/${projectId}/settings/style.css']`
-  )
-  if (projectStyle) {
-    projectStyle.remove()
+const projectSettingsPattern = new RegExp(
+  "https://scrapbox.io/projects/[^/]+/settings/"
+)
+
+if (
+  !url.includes("scrapbox.io/settings/profile") ||
+  !projectSettingsPattern.test(url)
+) {
+  const projectId = window.location.href.match(
+    RegExp("^https://scrapbox.io/(?<projectId>.*)/.*$")
+  ).groups.projectId
+
+  const interval = window.setInterval(() => {
+    const projectStyle = document.querySelector(
+      `link[href='/api/code/${projectId}/settings/style.css']`
+    )
+    if (projectStyle) {
+      projectStyle.remove()
+      window.clearInterval(interval)
+    }
+  }, 100)
+
+  window.setTimeout(() => {
     window.clearInterval(interval)
-  }
-}, 100)
-
-window.setTimeout(() => {
-  window.clearInterval(interval)
-}, 3000)
+  }, 3000)
+}
