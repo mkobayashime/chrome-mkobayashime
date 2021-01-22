a904823507fa05ae99cbdb2e492e93b069e74ed1
Masaki Kobayashi <contact@mkobayashi.me>
2021-01-23 02:16:17 +0900

    [Update] Add slack.js

---
+++ src/contentScripts/slack.js
@@ -0,0 +1,16 @@
+"use strict"
+
+window.addEventListener("beforeunload", (event) => {
+  const messageInputContainer = document.getElementsByClassName("ql-editor")[0]
+  if (messageInputContainer) {
+    const isEmpty = new RegExp(".")
+
+    const messageLines = messageInputContainer.children
+
+    if (Array.from(messageLines).some((line) => isEmpty.test(line.innerText))) {
+      event.preventDefault()
+      event.returnValue = ""
+      return false
+    }
+  }
+})
