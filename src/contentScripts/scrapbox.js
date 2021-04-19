6b0f7ea3be5592457624c782a323bc191fcd39e9
Masaki Kobayashi <contact@mkobayashi.me>
2021-04-19 17:17:02 +0900

    Disable scrapbox.js so far

--- src/contentScripts/scrapbox.js
+++ src/contentScripts/scrapbox.js
@@ -1,27 +1,27 @@
 "use strict"
 
-const url = window.location.href
+// const url = window.location.href
 
-const settingPagesPattern = new RegExp(
-  "https://scrapbox.io/(projects/[^/]+/)?settings/"
-)
+// const settingPagesPattern = new RegExp(
+//   "https://scrapbox.io/(projects/[^/]+/)?settings/"
+// )
 
-if (!settingPagesPattern.test(url)) {
-  const projectId = window.location.href.match(
-    RegExp("^https://scrapbox.io/(?<projectId>.*)/.*$")
-  ).groups.projectId
+// // if (!settingPagesPattern.test(url)) {
+// //   const projectId = window.location.href.match(
+// //     RegExp("^https://scrapbox.io/(?<projectId>.*)/.*$")
+// //   ).groups.projectId
 
-  const interval = window.setInterval(() => {
-    const projectStyle = document.querySelector(
-      `link[href='/api/code/${projectId}/settings/style.css']`
-    )
-    if (projectStyle) {
-      projectStyle.remove()
-      window.clearInterval(interval)
-    }
-  }, 100)
+// //   const interval = window.setInterval(() => {
+// //     const projectStyle = document.querySelector(
+// //       `link[href='/api/code/${projectId}/settings/style.css']`
+// //     )
+// //     if (projectStyle) {
+// //       projectStyle.remove()
+// //       window.clearInterval(interval)
+// //     }
+// //   }, 100)
 
-  window.setTimeout(() => {
-    window.clearInterval(interval)
-  }, 3000)
-}
+// //   window.setTimeout(() => {
+// //     window.clearInterval(interval)
+// //   }, 3000)
+// // }
